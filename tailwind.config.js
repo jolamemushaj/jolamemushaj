ee309458469e4260645fcb642f60dc123d07a8a9
jolamemushaj <jolamemushaj@gmail.com>
2024-04-09 13:02:47 +0200

    Page redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'primary': ['PlayfairDisplay', 'serif'],
+        'secondary': ['PoppinsLight', 'sans'],
+
+        'ternary': ['Poppins', 'sans'],
+      },
+      colors: {
+        'primary': '#DCD4CB',
+        'secondary': '#F7F1E3',
+        'ternary': 'rgba(220,210,208,0.61)',
+        'neutral': '#F4F1F1',
+      },
+    },
+
   },
   plugins: [],
 }
